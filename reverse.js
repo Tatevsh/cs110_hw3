142dd376b0cec7acf548abcabe5c84d6a230951d
Tatevsh <tatewsh@gmail.com>
2016-10-18 23:02:44 +0400

    Create reverse.js

---
+++ reverse.js
@@ -0,0 +1,10 @@
+const reverse = function(input) {
+	let rev = new Array;
+	for (let i = input.lenght - 1; i >= 0; i--) {
+		rev.push(input[i]);
+	}
+	return ret;
+}
+
+const reversedArray = reverse([1, 2, 3, 4, 5, 6, 7]);
+console.log(reversedArray);
